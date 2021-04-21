7f580a5bbb0a7d0599c3b19757b5236b178a2ef4
Michael Richards <fluffy.trooper.2001@gmail.com>
2021-04-21 15:10:20 +1000

    snek

---
+++ sus/cursed.py
@@ -0,0 +1,32 @@
+@lambda _0:_0()
+class cout:
+ __rgt__=__lt__=print
+ __rrshift__=__lshift__=lambda _0,_1:print(end=str(_1))or _0
+ __rge__=__le__=lambda _0,_1:print(end=str(_1))or _1
+ __str__=__repr__=lambda _:'std::cout'
+ __pos__=__neg__=__invert__=lambda _0:_0
+
+def g(u,___):
+    _=___(u).read();__=____(_,features="html.parser");_={}
+    for l in __.findAll(chr(97)):
+        try:___=l.get('href');assert ___.startswith(_____[()==()]);_.update({___:eval(....__ge__(...).__str__()[:(....__eq__(...)).__add__(....__eq__(...))]+....__ge__(...).__str__()[((....__eq__(...)<<....__eq__(...))<<....__eq__(...)).__neg__()-(....__eq__(...)):((....__eq__(...)<<....__eq__(...))<<....__eq__(...)).__neg__()+(....__eq__(...))][....__eq__(...)]+....__ge__(...).__str__()[....__ne__(...)-(((....__eq__(...)<<....__eq__(...))<<....__eq__(...))>>....__eq__(...))])})
+        except:...
+    for s in __(["s","style"]):s.extract()
+    __=__.get_text();______=(___.strip()for ___ in __.splitlines());_______=(________.strip()for ___ in ______ for ________ in ___.split(chr((()==())<<5)*2));__=chr(10).join(___ for ___ in _______ if ___);return __,_
+def m(_):
+    print(_)
+    global _____;_____[()!=()]=_.rstrip(_.split('/')[-(()==())]).rstrip('/').__add__(_____[()==()]);_________________,_l=g(_,___);c=dict();f=(()!=());_,n='',str();______=print
+    for l in _l:
+      try:
+        _t,_________________=g(l if l.startswith(_____[(()!=())])else _____[(()!=())]+l.lstrip(_____[()==()]),___);t=_t.find("pounds")-((()==())<<....__eq__(...));_l[l]=_t[:_t.find('|')-(()==())].strip()
+        while _t[t].isdigit():t-=(()==())
+        t+=(()==());d=str()
+        while _t[t].isdigit():d+=_t[t];t+=(...==...)
+        w=int(d);c.update({l:w})
+        if w>=f:f=w;_=l;n=_l[l]
+        ______(_l[l],end=', ')
+      except Exception as ____:______('['+str(____),l+']',end=', ')
+    #if _.startswith(_____[()==()]):_= _____[()!=()].__add__(_.lstrip(_____[()==()]))
+    if _:______('\b\b\nFattest cat detected [link, weight, name]:');return[_,f,n]
+    else:return....__ge__(...).__str__()[:(....__eq__(...)).__add__(....__eq__(...))]+....__ge__(...).__str__()[((....__eq__(...)<<....__eq__(...))<<....__eq__(...)).__neg__()-(....__eq__(...)):((....__eq__(...)<<....__eq__(...))<<....__eq__(...)).__neg__()+(....__eq__(...))][....__eq__(...)]+....__ge__(...).__str__()[....__ne__(...)-(((....__eq__(...)<<....__eq__(...))<<....__eq__(...))>>....__eq__(...))]
+__=(()==()).__add__(....__eq__(...));____=(__<<__<<__<<__);___=____>>(()==());x=__.__add__(__).__add__(__);h=____-x*(__**__);t=h-((()==())<<__)+(__.__mul__(__)).__pow__(__);p=t-x+__;s=p+x.__floordiv__(__);c=___-x;f=___-x*(x-(()==()))//__-__;a=f.__mul__(__)+__.__add__(__.__eq__(__));w=s+__.__pow__(__);l=p-__.__pow__(__);d=f-(()==());o=p-(()==());r=p.__add__(__);e=a+__.__mul__(__);b=a+(()==());q=f-__;_____=[];_g=a+x;_____.append(''.join([chr(int(_))for _ in [h,t,t,p,s,c,f,f,a,a,w,l,d,o,r,_g,f,p,e,t,f]]));_____.append(_____[()!=()][(x-(()==())).__neg__():]);from urllib.request import urlopen as ___;from bs4 import BeautifulSoup as ____;M=(m((_____[()!=()].rstrip(_____[()==()][()==():]))+''.join([chr(int(_))for _ in [f,a,a+x/__,o,p,t,a,b,l,e,q,a+__,a,t,s]])));N=....__str__()[()==():()==()].join([chr(int(_)) for _ in [h,t,t,p,s,c,f,f,a,a,w,l,d,o,r,_g]]);M[(()!=())]=N+M[(()!=())];cout<M
